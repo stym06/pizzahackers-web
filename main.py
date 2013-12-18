260ab0f449aa546c6024ad2d1db33ffa83a1632e
Sanket Saurav <sanketsaurav@gmail.com>
2013-12-18 13:05:42 +0530

    Flaskified

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+# sys.path includes 'server/lib' due to appengine_config.py
+from flask import Flask
+from flask import render_template
+app = Flask(__name__.split('.')[0])
+
+
+@app.route('/')
+def home():
+  """ Return index template at application root URL."""
+  return render_template('index.html')
+
+
