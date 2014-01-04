34702d7f1f484c73cc99186caa0f081d63a4cda6
Sanket Saurav <sanketsaurav@gmail.com>
2014-01-04 21:30:31 +0530

    Added more handlers and intiialized templates. Added introduction in join.html. Pushed missing gears.png.

--- main.py
+++ main.py
@@ -2,14 +2,32 @@ import os
 import sys
 
 # sys.path includes 'server/lib' due to appengine_config.py
+
 from flask import Flask
 from flask import render_template
 app = Flask(__name__.split('.')[0])
 
-
 @app.route('/')
 def home():
-  """ Return index template at application root URL."""
-  return render_template('home.html')
+	""" Return index template at application root URL."""
+	return render_template('home.html')
 
+@app.route('/login')
+def login():
+	return render_template('login.html')
 
+@app.route('/join')
+def join():
+	return render_template('join.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/rules')
+def rules():
+	return render_template('rules.html')
+
+@app.route('/hacks')
+def hacks():
+	return render_template('hacks.html')
