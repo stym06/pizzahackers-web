78b4894c1411b5a168931e0bcba0d910527965a4
Sanket Saurav <sanketsaurav@gmail.com>
2014-02-27 05:22:19 +0530

    Adding STATIC_ROOT.

--- pizzahackers/settings.py
+++ pizzahackers/settings.py
@@ -91,7 +91,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 
 # List of finder classes that know how to find static files in
